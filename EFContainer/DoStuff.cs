73ad2c6ad6abdc7b40a1507146279ca3b3ef3b7a
Brian Lachniet <blachniet@gmail.com>
2013-07-11 11:18:21 -0400

    Initial Commit

---
+++ EFContainer/DoStuff.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFContainer
+{
+    class DoStuff
+    {
+        public DoStuff()
+        {
+            var rand = new Random();
+            using (var ctx = new FooContext())
+            {
+                ctx.Bars.Add(new Bar()
+                {
+                    Name = Bars[rand.Next(Bars.Length)]
+                });
+                ctx.SaveChanges();
+            }
+        }
+
+        private static string[] Bars = {
+            "Tales and Spirits",
+            "Zensai",
+            "Cicchetti Bar at Piccolino",
+            "iKandy",
+            "Wink",
+            "Beaufort Bar",
+            "Jefrey's",
+            "Little Red Pocket",
+            "Hyde & Co",
+            "Strange Wolf"
+        };
+    }
+}
---
+++ EFMultiSchemaVersionTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFMultiSchemaVersionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loading assemblies...");
+
+            var loadedAssemblies = new Dictionary<string, Assembly>();
+            
+            var filesToLoad = Directory.GetFiles(Directory.GetCurrentDirectory(), "EFContainer*.dll");
+            //filesToLoad = filesToLoad.Reverse().ToArray();    // Why does this make it work???
+            foreach (var file in filesToLoad)
+            {
+                var assembly = Assembly.LoadFile(file);
+                var filename = Path.GetFileName(file);
+                loadedAssemblies.Add(filename, assembly);
+                Console.WriteLine("Loaded {0} from {1}", assembly.FullName, filename);
+            }
+
+            Console.WriteLine("Assemblies loaded. Press any key to insert a new row.");
+            Console.ReadKey();
+            Console.WriteLine("Inserting a new row in the Bars table.");
+
+            try
+            {
+                // Initializing an instance of this class should insert a new row in the Bars table
+                var dostuffer = loadedAssemblies["EFContainer.2.0.0.0.dll"].CreateInstance("EFContainer.DoStuff");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Exception while inserting new row: " + ex.ToString());
+            }
+
+            Console.WriteLine("Done");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
